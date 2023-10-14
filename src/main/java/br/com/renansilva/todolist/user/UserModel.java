07b4e8cdd924af866ff9ce076a4adb2cb0143ada
renan <renansilvademelo1995@gmail.com>
2023-10-13 22:01:27 -0300

    Dockerfile deplay

---
+++ src/main/java/br/com/renansilva/todolist/user/UserModel.java
@@ -0,0 +1,37 @@
+package br.com.renansilva.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+// @Data do lombok faz os getters e setters automaticamente
+// @Entity transforma em tablema para um bando de dados
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    
+     
+    
+
+    
+} 
